828443bdc6404368aeb5f60ae494aef2b2afdd69
Tomas Izuel <tomasizuel@gmail.com>
2024-06-26 10:21:08 -0300

    to merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,13 +9,9 @@ import configuration from './config/configuration';
 import { ProviderModule } from './provider/provider.module';
 import { OrderStatusModule } from './order-status/order-status.module';
 import { databaseProviders } from './config/mysql/mysql';
-<<<<<<< HEAD
-import { ProviderArticleService } from './provider-article/provider-article.service';
 import { ProviderArticleModule } from './provider-article/provider-article.module';
-=======
 import { ClientModule } from './client/client.module';
 import { SaleModule } from './sale/sale.module';
->>>>>>> develop
 
 @Module({
   imports: [
@@ -23,14 +19,11 @@ import { SaleModule } from './sale/sale.module';
     FamilyModule,
     ArticleModule,
     DatabaseModule,
-<<<<<<< HEAD
     ProviderModule,
     OrderStatusModule,
     ProviderArticleModule,
-=======
     ClientModule,
     SaleModule,
->>>>>>> develop
   ],
   controllers: [AppController],
   providers: [AppService, ...databaseProviders],
