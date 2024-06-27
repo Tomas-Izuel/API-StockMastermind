b58d875ac0dc44bb46f67bdd6d67585ad6911370
NinoPassera <86748919+NinoPassera@users.noreply.github.com>
2024-06-27 11:38:21 -0300

    Update create-predicted-demand.dto.ts

--- src/predicted-demand/dto/create-predicted-demand.dto.ts
+++ src/predicted-demand/dto/create-predicted-demand.dto.ts
@@ -1,7 +1,6 @@
 export interface CreatePredictedDemandDto {
     period: Date;
     quantity: number;
-    price_proyected: number;
     calculated_error: number;
     selected: boolean;
     demand_param_id: number;
